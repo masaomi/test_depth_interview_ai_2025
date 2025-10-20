5e83c8407ee5a8889aded542d0bf3ef6d796df0c
masaomi <masaomi.hatakeyama@gmail.com>
2025-10-20 20:05:41 +0200

    Fixed next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: process.cwd(), // 現在のディレクトリをルートとして明示的に指定
+  },
 };
 
 export default nextConfig;
